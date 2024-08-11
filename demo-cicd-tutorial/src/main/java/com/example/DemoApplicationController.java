ae52f646b1989165f558e6dac698a84594a88726
ghanshyam shukla <gsgmca682@gmail.com>
2024-08-11 15:23:20 +0530

    initial code committed-4

--- demo-cicd-tutorial/src/main/java/com/example/DemoApplicationController.java
+++ demo-cicd-tutorial/src/main/java/com/example/DemoApplicationController.java
@@ -4,10 +4,9 @@ package com.example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 public class DemoApplicationController {
